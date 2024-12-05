e99420d18c0895d65b1c0717d5498797b4b543d9
alan <alanleeweijun8@gmail.com>
2024-12-05 15:05:21 +0800

    Added Qn2 and fixed some bugs

--- question2.js
+++ question2.js
@@ -21,7 +21,7 @@ overlay.addEventListener("click", function () {
     overlay.style.display = "none";
 });
 
-const button2 = document.getElementById("correctAnswer");
+const button2 = document.getElementById("Option2");
 const popup2 = document.getElementById("popup2");
 const overlay2 = document.getElementById("overlay2");
 
@@ -53,7 +53,7 @@ if (!localStorage.getItem('score')) {
   scoreDisplay.textContent = localStorage.getItem('score');
 
 
-  document.getElementById('correctAnswer').addEventListener('click', () => {
+  document.getElementById('Option2').addEventListener('click', () => {
     let score = parseInt(localStorage.getItem('score'), 10);
     score += 5; 
     localStorage.setItem('score', score); 
